f3bced2aa109001760d2eb6483daf34bd67951be
Jay-Jiang <1540491116@qq.com>
2019-10-29 13:40:27 +0800

    springcould 消费者模块（Bean配置类）

---
+++ spring-cloud-consumer-80/src/main/java/com/demo/springcloud/config/ConfigBean.java
@@ -0,0 +1,27 @@
+package com.demo.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * _@Author: jianghj
+ * _@Date: 2019.10.28 028 16:19
+ * _@Desc: 将 Spring 封装的 RestTemplate 对象注入到 Spring 容器中。
+ */
+@Configuration
+public class ConfigBean {
+
+	public static String DEPT_URL_PREFFIX;
+
+	@Value("${dept.url.preffix}")
+	private void setDeptUrlPreffix(String deptUrlPreffix){
+		DEPT_URL_PREFFIX = deptUrlPreffix;
+	}
+
+	@Bean("restTemplate")
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+}
