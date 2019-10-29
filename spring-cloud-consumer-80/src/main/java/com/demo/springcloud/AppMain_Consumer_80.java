a5e6c057214d8bf9296979ba6f0f9e718a4ee328
Jay-Jiang <1540491116@qq.com>
2019-10-29 13:37:33 +0800

    springcould 消费者模块

---
+++ spring-cloud-consumer-80/src/main/java/com/demo/springcloud/AppMain_Consumer_80.java
@@ -0,0 +1,17 @@
+package com.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * _@Author: jianghj
+ * _@Date: 2019.10.28 028 17:35
+ * _@Desc:
+ */
+@SpringBootApplication
+public class AppMain_Consumer_80 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMain_Consumer_80.class, args);
+	}
+}
---
+++ spring-cloud-consumer-80/src/main/java/com/demo/springcloud/controller/DeptConsumerController.java
@@ -0,0 +1,44 @@
+package com.demo.springcloud.controller;
+
+import com.demo.springcloud.config.ConfigBean;
+import com.demo.springcloud.entity.Dept;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * _@Author: jianghj
+ * _@Date: 2019.10.28 028 16:24
+ * _@Desc: RESTful 风格的消费
+ */
+@RestController
+public class DeptConsumerController {
+
+	@Resource
+	RestTemplate restTemplate;
+
+	@RequestMapping(value = "/consumer/dept/add", method = RequestMethod.POST)
+	public Boolean addNewDept(@RequestBody Dept dept) {
+		return restTemplate.postForObject(ConfigBean.DEPT_URL_PREFFIX + "add", dept, Boolean.class);
+	}
+
+	@RequestMapping(value = "/consumer/dept/get/{deptNo}", method = RequestMethod.GET)
+	public Dept getDept(@PathVariable("deptNo") Long deptNo) {
+		return restTemplate.getForObject(ConfigBean.DEPT_URL_PREFFIX+"get/{1}", Dept.class, deptNo);
+	}
+
+	@RequestMapping(value = "/consumer/dept/list", method = RequestMethod.GET)
+	public List<Dept> list() {
+		return restTemplate.getForObject(ConfigBean.DEPT_URL_PREFFIX + "list", List.class);
+	}
+
+	@RequestMapping(value = "/consumer/dept/delete/{deptNo}", method = RequestMethod.DELETE)
+	public Boolean deleteDept(@PathVariable("deptNo") Long deptNo) {
+		restTemplate.delete(ConfigBean.DEPT_URL_PREFFIX + "delete/"+ deptNo);
+		return true;
+	}
+
+
+}
