e85db1bdb3c4edf00010525b7a1527afb334ba3b
Jay-Jiang <1540491116@qq.com>
2019-11-11 17:27:49 +0800

    springcould 服务提供者（基于Hystrix的服务熔断）

--- spring-cloud-provider-hystrix-8004/src/main/java/com/demo/springcloud/AppMain_Dept_Hystrix_8004.java
+++ spring-cloud-provider-hystrix-8004/src/main/java/com/demo/springcloud/AppMain_Dept_Hystrix_8004.java
@@ -1,9 +1,12 @@
 package com.demo.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * _@Author: jianghj
@@ -17,5 +20,19 @@ public class AppMain_Dept_Hystrix_8004 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppMain_Dept_Hystrix_8004.class, args);
+	}
+
+	/**
+	 * 用于发现 /hystrix.stream，避免访问时 404 错误
+	 * SpringBoot 2.0 的 bug 之一
+	 */
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 }
