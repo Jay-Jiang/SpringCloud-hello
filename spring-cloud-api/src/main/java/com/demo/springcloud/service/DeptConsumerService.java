8e579566f1bd414c02995aaa291f61b3eb75600b
Jay-Jiang <1540491116@qq.com>
2019-11-11 14:04:11 +0800

    springcould 通用模块（接口、实体、工具类等）

--- spring-cloud-api/src/main/java/com/demo/springcloud/service/DeptConsumerService.java
+++ spring-cloud-api/src/main/java/com/demo/springcloud/service/DeptConsumerService.java
@@ -2,6 +2,7 @@ package com.demo.springcloud.service;
 
 import com.demo.springcloud.entity.Dept;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,8 +11,8 @@ import java.util.List;
 /**
  * _@Author: jianghj
  * _@Date: 2019.11.5 005 17:32
- *  按指定的服务名称，为该服务添加客户端负载均衡的接口（默认是轮询算法）
- *  其中，接口方法上，必须指定访问路径
+ * 按指定的服务名称，为该服务添加客户端负载均衡的接口（默认是轮询算法）
+ * 其中，接口方法上，必须指定访问路径
  */
 @FeignClient("PROVIDER-DEPT")
 public interface DeptConsumerService {
@@ -20,11 +21,11 @@ public interface DeptConsumerService {
 	Boolean addDept(Dept dept);
 
 	@RequestMapping(value = "/dept/get/{deptNo}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-	Dept getDept(Long deptNo);
+	Dept getDept(@PathVariable("deptNo") Long deptNo);
 
 	@RequestMapping(value = "/dept/list", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
 	List<Dept> listDept();
 
 	@RequestMapping(value = "/dept/delete/{deptNo}", method = RequestMethod.DELETE, produces = "application/json;charset=UTF-8")
-	Boolean deleteDept(Long deptNo);
+	Boolean deleteDept(@PathVariable("deptNo") Long deptNo);
 }
