1e83c80a984c4034bae0584dc27e1b31e51fc3b2
Jay-Jiang <1540491116@qq.com>
2019-11-14 16:16:53 +0800

    springcould 配置中心服务器 Config Server

---
+++ spring-cloud-config-server-9526/src/main/java/com/demo/springcloud/AppMain_Config_9526.java
@@ -0,0 +1,19 @@
+package com.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * _@Author: jianghj
+ * _@Date: 2019.10.28 028 15:31
+ * _@Desc: 开启网关 Zuul
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class AppMain_Config_9526 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMain_Config_9526.class, args);
+	}
+}
